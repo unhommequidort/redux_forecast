640496e37ba854880707fc177150741fead3ce5c
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-08-20 17:05:34 -0500

    Write weather api ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'fd82ff628ccdeeea55cfc198c1c9da0f';
+
+const ROOT_URL = `http://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
